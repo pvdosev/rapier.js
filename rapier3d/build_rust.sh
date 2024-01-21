79d2a53face1b217f104cda8f83647e30de3dd69
Petar Dosev <pdosev@gmail.com>
2024-01-21 14:34:59 -0500

    Uglify build system

--- rapier3d/build_rust.sh
+++ rapier3d/build_rust.sh
@@ -5,3 +5,8 @@ sed -i.bak 's#dimforge_rapier#@dimforge/rapier#g' pkg/package.json
 sed -i.bak 's/"rapier_wasm3d_bg.wasm"/"*"/g' pkg/package.json
 rm pkg/*.bak
 rm pkg/.gitignore
+
+# a slightly ugly way to munge the files to work with my bundler
+sed -i '2,3d'  pkg/rapier_wasm3d.js
+sed -i '1,4d' pkg/rapier_wasm3d_bg.js
+sed -i '1s!.*!import * as wasm from "./rapier_wasm3d_bg.wasm";!' pkg/rapier_wasm3d_bg.js
